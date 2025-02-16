06553b30765a5c5b0c08786893c907d869472580
Jean Freitas <jeannsfreitas@gmail.com>
2025-02-15 21:18:45 -0300

    feat(ui): add button components

--- client/src/components/ListHeader.tsx
+++ client/src/components/ListHeader.tsx
@@ -6,10 +6,21 @@ interface ListHeaderProps {
 }
 
 const ListHeader: React.FC<ListHeaderProps> = ({ listName }) => {
+const signOut = () => {
+  console.log('signout')
+};
+
+
   return (
     <div className="list-header">
-      <FaCheckSquare className="list-header-icon" size={30} />
-      <h1 className="list-header-title">{listName}</h1>
+      <h1 className="list-header-title">
+        <FaCheckSquare className="list-header-icon" size={30} /> {listName}
+      </h1>
+      <div className='button-container'>
+
+      <button className="create">Adicionar</button>
+      <button className='signout' onClick={signOut}>Sair</button>
+      </div>
     </div>
   );
 };
